26e1cd27ff27ee921a3b157e1e3b6b807b7fdcc3
Nina <nina.kostenko.js@gmail.com>
2024-10-23 21:54:47 +0300

    header component

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {cn} from "@/lib/utils";
+import {Container} from "@/components/shared/container";
+import Image from "next/image";
+import {Button} from "../ui";
+import {ArrowRight, ShoppingCart, UserRound} from "lucide-react";
+
+interface Props {
+    className?: string
+}
+
+export const Header: React.FC<Props> = ({className}) => {
+    return (
+        <header className={cn('border border-b', className)}>
+            <Container className="flex items-center justify-between py-8">
+                <div className="flex items-center gap-4">
+                    <Image src="/logo.png" alt="Logo" width={35} height={35}/>
+                    <div>
+                        <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+                        <p className="text-sm text-gray-400 leading-3">most delicious</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-1">
+                    <Button variant='outline' className="flex items-center gap-3">
+                        <UserRound size={16}/>
+                        Enter</Button>
+                    <div>
+                        <Button className="group relative">
+                            <b>20$</b>
+                            <span className="h-full w-[1px] bg-white/30 mx-3"/>
+                            <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                                <ShoppingCart size={16} className="relative" strokeWidtht={2}/>
+                                <b>0</b>
+                            </div>
+                            <ArrowRight size={20}
+                                        className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"/>
+                        </Button>
+                    </div>
+                </div>
+            </Container>
+        </header>
+    );
+};
+
